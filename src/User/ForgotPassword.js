128de71cfc8d964c3282e1f281eae957c91a79c6
deekshad060-a11y <deekshad060@gmail.com>
2025-10-16 07:30:44 +0530

    Update ForgotPassword.js

--- src/User/ForgotPassword.js
+++ src/User/ForgotPassword.js
@@ -8,7 +8,7 @@ export default function ForgotPasswordForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("https://clinigoal2025.onrender.com/auth/forgot-password", {
+      const res = await fetch("http://localhost:5000/auth/forgot-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
